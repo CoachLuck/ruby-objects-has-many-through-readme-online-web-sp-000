fc44e1209e3f8b2c2943b4a06b8bb286a899b036
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 23:07:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,31 +19,7 @@ class Waiter
     end
   end
 
-  def waiters
-    meals.map do |meal|
-      meal.waiter
-    end
-  end
-
   def new_meal(customer, total, tip=0)
     Meal.new(customer, self, total, tip)
   end
-
-  def new_meal_20_percent(waiter, total)
-    tip = total * 0.2
-    Meal.new(customer, self, total, tip)
-  end
-
-  def self.oldest_waiter
-    oldest_age = 0
-    oldest_waiter = nil
-    self.all.each do |waiter|
-      if waiter.age > oldest_age
-        oldest_age = waiter.age
-        oldest_waiter = waiter
-      end
-    end
-    oldest_waiter
-  end
-
 end
